e842306691d2159e8d6dd7951b157edb1606513a
Olorundare Micheal <meshelleva@gmail.com>
2024-04-20 12:28:03 +0100

    update routes and added scheduler to change information status after 1o days

--- utility/app_package/database.py
+++ utility/app_package/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Evayoung%4030@localhost/dclm'
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres.pneysjdvpjbniwqafrou:dclmkwarastate2024@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Evayoung%4030@localhost/dclm'
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.pneysjdvpjbniwqafrou:dclmkwarastate2024@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
